afd579b2a5cc1ef6360cc8f5ad6e368b877170f9
Egor <ekurito@gmail.com>
2016-09-17 18:17:40 +0300

    bugfix

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import StringField, TextField, BooleanField
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+    openid = TextField('openid', validators = [DataRequired()])
     remember_me = BooleanField('remember_me', default = False)
